ea8ee2f90e8bbaecf016da890064a0bbb69d9469
murvancev39 <murvancev39@gmail.com>
2025-08-23 16:54:15 +0300

    added switch

--- square.cpp
+++ square.cpp
@@ -4,20 +4,42 @@
 const double EPS = 1e-9;
 
 int sravnenie(double a);
-void linear(double a, double b, double c, double* x1, double* x2);
-void quadratick (double a, double b, double c, double* x1, double* x2);
+void linear(double a, double b, double c, double* x1, double* x2, int* nRoots);
+void quadratick (double a, double b, double c, double* x1, double* x2, int* nRoots);
 
 int main()
 {
     printf("ax**2 + bx + c = 0\n");
     printf("vvedite a, b, c\n");
 
+    int nRoots = 0;
+
     double a = 0, b = 0, c = 0;
     scanf("%lf, %lf, %lf", &a, &b, &c);
 
     double x1 = 0, x2 = 0;
 
-    quadratick(a, b,  c, &x1, &x2);
+    quadratick(a, b,  c, &x1, &x2, &nRoots);
+    
+    switch (nRoots)
+    {
+        case -1:
+            printf("INFINITY ROOTS");
+            break;
+        case 0:
+            printf("NO ROOTS");
+            break;
+        case 1:
+            printf("ONE ROOT IS %lg", x1);
+            break;
+        case 2:
+            printf("TWO ROOTS: X1 = %lg AND X2 = %lg", x1, x2);
+            break;
+        default:
+            printf("ITS IMPOSSIBLE");
+            break;
+    }
+
     return 0;
 }
 
@@ -35,35 +57,33 @@ int sravnenie(double a)
 
 
 
-void linear(double a, double b, double c, double* x1, double* x2)
+void linear(double a, double b, double c, double* x1, double* x2, int* nRoots)
 {
     if (sravnenie(b) < 0)
     {
         if (sravnenie(c) < 0)
         {
-            printf("beskonechno resheniy");
+            *nRoots = -1;
         }
         else
         {
-            printf("net resheniy");
+            *nRoots = 0;
         }
     }
     else
     {
-        printf("ti daun?");
-        *x1 = *x2 = (-c / b); // FIXME
-        printf(" eto ne qvadratnoye uravnenie , a lineynoye i imeet odno resheniye\n");
-        printf("x = %lf", *x1);
+        *nRoots = 1;
+        *x1 = *x2 = (-c / b);
     }
 }
 
 
 
-void quadratick (double a, double b, double c, double* x1, double* x2)
+void quadratick (double a, double b, double c, double* x1, double* x2, int* nRoots)
 {
     if (sravnenie(a) < 0)
     {
-        linear(a, b, c, x1, x2);
+        linear(a, b, c, x1, x2, nRoots);
         return;
     }
     
@@ -71,17 +91,17 @@ void quadratick (double a, double b, double c, double* x1, double* x2)
     if (sravnenie(D) < 0)
     {
         *x1 = *x2 = -b / (2 * a);
-        printf("x1 = x2 = %lf", *x1);
+        *nRoots = 1;
     }
     else if (D < 0)
     {
-        printf("no solution");
+        *nRoots = 0;
     }
     else
     {
         *x1 = (-b + sqrt(D)) / (2 * a);
         *x2 = (-b - sqrt(D)) / (2 * a);
-        printf("x1 = %lf, x2 = %lf", *x1, *x2);
+        *nRoots = 2;
     }
     
 }
