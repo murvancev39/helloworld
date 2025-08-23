85124df17926c58eaf55232eb667c1af349ae2d5
murvancev39 <murvancev39@gmail.com>
2025-08-23 14:50:13 +0300

    split into function

--- square.cpp
+++ square.cpp
@@ -4,6 +4,8 @@
 const double EPS = 1e-9;
 
 int sravnenie(double a);
+void linear(double a, double b, double c, double* x1, double* x2);
+void quadratick (double a, double b, double c, double* x1, double* x2);
 
 int main()
 {
@@ -17,46 +19,16 @@ int main()
 
     if (sravnenie(a) < 0)
     {
-        if (sravnenie(b) < 0)
-        {
-            if (sravnenie(c) < 0)
-            {
-                printf("beskonechno resheniy");
-            }
-            else
-            {
-                printf("net resheniy");
-            }
-        }
-        else
-        {
-            printf("ti daun?");
-            x1 = x2 = -c / b; // FIXME
-            printf(" eto ne qvadratnoye uravnenie , a lineynoye i imeet odno resheniye\n");
-            printf("x = %lf", x1);
-        }
+        linear(a, b, c, &x1, &x2);
     }
     else
     {
-        double D = b * b - 4 * a * c;
-        if (sravnenie(D) < 0)
-        {
-            x1 = x2 = -b / (2 * a);
-            printf("x1 = x2 = %lf", x1);
-        }
-        else if (D < 0)
-        {
-            printf("no solution");
-        }
-        else
-        {
-            x1 = (-b + sqrt(D)) / (2 * a);
-            x2 = (-b - sqrt(D)) / (2 * a);
-            printf("x1 = %lf, x2 = %lf", x1, x2);
-        }
+        quadratick(a, b, c, &x1, &x2);
     }
     return 0;
 }
+
+
 
 int sravnenie(double a)
 {
@@ -66,4 +38,50 @@ int sravnenie(double a)
     }
 
     return 1;
+}
+
+
+
+void linear(double a, double b, double c, double* x1, double* x2)
+{
+    if (sravnenie(b) < 0)
+    {
+        if (sravnenie(c) < 0)
+        {
+            printf("beskonechno resheniy");
+        }
+        else
+        {
+            printf("net resheniy");
+        }
+    }
+    else
+    {
+        printf("ti daun?");
+        *x1 = *x2 = (-c / b); // FIXME
+        printf(" eto ne qvadratnoye uravnenie , a lineynoye i imeet odno resheniye\n");
+        printf("x = %lf", x1);
+    }
+}
+
+
+
+void quadratick (double a, double b, double c, double* x1, double* x2)
+{
+    double D = b * b - 4 * a * c;
+        if (sravnenie(D) < 0)
+        {
+            *x1 = *x2 = -b / (2 * a);
+            printf("x1 = x2 = %lf", x1);
+        }
+        else if (D < 0)
+        {
+            printf("no solution");
+        }
+        else
+        {
+            *x1 = (-b + sqrt(D)) / (2 * a);
+            *x2 = (-b - sqrt(D)) / (2 * a);
+            printf("x1 = %lf, x2 = %lf", x1, x2);
+        }
 }
